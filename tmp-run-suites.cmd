a760def97e28545c3049902d815225c07c79ebd4
Game Parlour Owner <owner@gameparlour.com>
2026-09-24 01:13:29 +0530

    Cleanup: remove temporary test files

--- tmp-run-suites.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set "VERIFY_URL=http://localhost:3000"
-node tmp-img-proof2.mjs > proof2.log 2>&1
-node verify-func.mjs > verify-func.log 2>&1
-echo FUNC_EXIT=%ERRORLEVEL% >> verify-func.log
-node verify-nav.mjs > verify-nav.log 2>&1
-echo NAV_EXIT=%ERRORLEVEL% >> verify-nav.log
-echo DONE > suites.done
